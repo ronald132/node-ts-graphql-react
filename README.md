a93f274456a3c8a833087a41db9b6d40725006f2
ronald132 <ronald132@gmail.com>
2024-04-22 20:07:22 +0800

    mocking listing data and API

--- README.md
+++ README.md
@@ -17,3 +17,14 @@ node build/index.js
 ## Linsting with ESList
 
 npm i -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+
+## Enable body parser
+
+npm i -D @types/body-parser
+
+## Testing with curl
+
+curl -X POST http://l"ocalhost:9000/delete-listing -H 'Content-Type: application/json' -d '{"id": "001"}'
+
+-X : specify which HTTP method to use
+-H : header
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-
+import { listings } from './listings'
+import bodyParser from 'body-parser'
 const app = express()
 
 const port: number = 9000
@@ -7,7 +8,26 @@ const port: number = 9000
 const one: number = 1
 const two: number = 2
 
+// parsing incoming json request
+app.use(bodyParser.json())
+
 app.get('/', (_req, res) => res.send(`Hello world ${one + two}`))
+
+app.get('/listings', (_req, res) => {
+  return res.send(listings)
+})
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1))
+    }
+  }
+
+  return res.send('failed to delete listing')
+})
 
 app.listen(port)
 
